00ffdd65f05c506a45e6c852634da99c41f47245
Nick Allen <nick@nickallen.org>
2015-03-12 15:45:33 -0400

    Fixed whitespace issues

--- kibana.rb
+++ kibana.rb
@@ -6,32 +6,32 @@ class Kibana < Formula
   url 'https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-darwin-x64.tar.gz'
   sha1 '7729aba3ac26ec21ffc4a7b60046b638b076aa4a'
 
-	depends_on "elasticsearch" => :recommended
-	depends_on "logstash" => :recommended
+  depends_on "elasticsearch" => :recommended
+  depends_on "logstash" => :recommended
 
-	skip_clean 'src'
+  skip_clean 'src'
 
   def install
 
-		# remove windows specific files
+    # remove windows specific files
     rm_f Dir["bin/*.bat"]
 
-		prefix.install Dir['src']
-		prefix.install Dir['node']
+    prefix.install Dir['src']
+    prefix.install Dir['node']
 
-		# kibana configuration
-		etc.install 'config/kibana.yml'
+    # kibana configuration
+    etc.install 'config/kibana.yml'
 
-		# point to the kibana configuration 
-		inreplace 'bin/kibana', 
-			'"${DIR}/config/kibana.yml"', 
-			etc/'kibana.yml'
+    # point to the kibana configuration 
+    inreplace 'bin/kibana', 
+      '"${DIR}/config/kibana.yml"', 
+      etc/'kibana.yml'
 
-		# install the binary
+    # install the binary
     bin.install 'bin/kibana'
   end
 
-	def caveats; <<-EOS.undent
+  def caveats; <<-EOS.undent
     Kibana configuration: #{etc}/kibana.yml
     Ensure 'elasticsearch' is running
     Execute 'kibana'
